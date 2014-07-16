942e8f53d094c6598484b5446a214b0329475be6
Jon Hjelle <hjon@andyet.net>
2014-07-16 15:42:05 -0700

    Add JAHConvertJingle files

---
+++ JAHConvertJingle/JAHConvertJingle.h
@@ -0,0 +1,13 @@
+//
+//  JAHConvertJingle.h
+//  JAHConvertJingle
+//
+//  Created by Jon Hjelle on 7/16/14.
+//  Copyright (c) 2014 Jon Hjelle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JAHConvertJingle : NSObject
+
+@end
---
+++ JAHConvertJingle/JAHConvertJingle.m
@@ -0,0 +1,13 @@
+//
+//  JAHConvertJingle.m
+//  JAHConvertJingle
+//
+//  Created by Jon Hjelle on 7/16/14.
+//  Copyright (c) 2014 Jon Hjelle. All rights reserved.
+//
+
+#import "JAHConvertJingle.h"
+
+@implementation JAHConvertJingle
+
+@end
