5254e1c6ee8a03ea9a976487b17d43c124f88046
Jon Hjelle <hjon@andyet.net>
2014-07-16 15:39:14 -0700

    Initial Commit

---
+++ JAHConvertJingle/JAHAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  JAHAppDelegate.m
+//  JAHConvertJingle
+//
+//  Created by Jon Hjelle on 7/16/14.
+//  Copyright (c) 2014 Jon Hjelle. All rights reserved.
+//
+
+#import "JAHAppDelegate.h"
+
+@implementation JAHAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ JAHConvertJingle/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  JAHConvertJingle
+//
+//  Created by Jon Hjelle on 7/16/14.
+//  Copyright (c) 2014 Jon Hjelle. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
