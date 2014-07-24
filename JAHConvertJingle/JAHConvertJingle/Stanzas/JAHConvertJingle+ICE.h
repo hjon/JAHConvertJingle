5dc9117c5f0a4819c25b3dd18e1de75f392563dc
Jon Hjelle <hjon@andyet.net>
2014-07-24 13:06:36 -0700

    Add files for ICE

---
+++ JAHConvertJingle/JAHConvertJingle/Stanzas/JAHConvertJingle+ICE.h
@@ -0,0 +1,13 @@
+//
+//  JAHConvertJingle+ICE.h
+//  JAHConvertJingle
+//
+//  Created by Jon Hjelle on 7/24/14.
+//  Copyright (c) 2014 Jon Hjelle. All rights reserved.
+//
+
+#import "JAHConvertJingle.h"
+
+@interface JAHConvertJingle (ICE)
+
+@end
---
+++ JAHConvertJingle/JAHConvertJingle/Stanzas/JAHConvertJingle+ICE.m
@@ -0,0 +1,13 @@
+//
+//  JAHConvertJingle+ICE.m
+//  JAHConvertJingle
+//
+//  Created by Jon Hjelle on 7/24/14.
+//  Copyright (c) 2014 Jon Hjelle. All rights reserved.
+//
+
+#import "JAHConvertJingle+ICE.h"
+
+@implementation JAHConvertJingle (ICE)
+
+@end
